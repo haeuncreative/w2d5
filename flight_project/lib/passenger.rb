9a29027e5c90491c749b56341e768d26a41ec346
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-10-11 19:58:42 -0700

    finished passenger

--- flight_project/lib/passenger.rb
+++ flight_project/lib/passenger.rb
@@ -8,25 +8,29 @@ class Passenger
     end
 
     def has_flight?(string)
-        if self.flight_numbers.include?(string)
+        valid_str = make_valid(string)
+        if @flight_numbers.include?(valid_str)
             return true
+        else
+            return false
         end
-        return false
     end
 
+    def make_valid(string)
+        chars = string.split(" ")
+        chars.map! do |ele| 
+            if ele.is_a?(String)
+                ele.upcase
+            end
+        end
+        string = chars.join("")
+        return string
+    end
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def add_flight(string)
+        if !has_flight?(string)
+            valid_str = make_valid(string)
+            @flight_numbers << valid_str
+        end    
+    end
 end
